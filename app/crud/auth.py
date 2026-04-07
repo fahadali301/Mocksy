4e0fb69c6f659c7932bc55c1dd992b3f38ef3149
itxmuhammadwaqarali <itxmewaqar@gmail.com>
2026-04-07 17:28:49 +0500

    replace datetime.utcnow() with datetime.now()

--- app/crud/auth.py
+++ app/crud/auth.py
@@ -15,6 +15,6 @@ def verify_password(plain, hashed):
 
 def create_token(data: dict):
     to_encode = data.copy()
-    expire = datetime.utcnow() + timedelta(minutes=60)
+    expire = datetime.now() + timedelta(minutes=60)
     to_encode.update({"exp": expire})
     return jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
