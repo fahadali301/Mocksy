f000948bf4ad5630abcd7eaf30ac1e2c3c764dcf
itxmuhammadwaqarali <itxmewaqar@gmail.com>
2026-04-09 15:33:59 +0500

    configure DATABASE_URL in alembic environment

--- alembic/env.py
+++ alembic/env.py
@@ -6,10 +6,12 @@ from sqlalchemy import pool
 from alembic import context
 
 from app.core.database import Base
+from app.core.config import DATABASE_URL
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
+config.set_main_option("sqlalchemy.url", DATABASE_URL)
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
